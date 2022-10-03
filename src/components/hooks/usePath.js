d72eef873e7f8fa434dc215c3e2272e03d54bdf3
Gustavo <gustgouvea@hotmail.com>
2022-10-03 11:06:43 -0300

    começo custom, revizar bug

---
+++ src/components/hooks/usePath.js
@@ -0,0 +1,13 @@
+import { useLocation } from "react-router"
+
+export default function usePath() {
+    let isHome = false;
+
+    const { pathname } = useLocation();
+
+    if (pathname === '/' || pathname === '/#') isHome = true;
+
+    return {
+        isHome,
+    };
+}
